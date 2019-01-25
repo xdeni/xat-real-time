a1ae845bc86b48470e5c8b0723e841996b6ff7e3
Adenilson Santos <adenilsonblog@gmail.com>
2019-01-24 21:11:55 -0300

    finalized

--- server.js
+++ server.js
@@ -15,8 +15,18 @@ app.use("/", (req, res) => {
   res.render("index.html");
 });
 
+const messages = [];
+
 io.on("connection", socket => {
   console.log("Socket conectado: ", socket.id);
+
+  socket.emit("previousMessages", messages);
+
+  socket.on("sendMessage", data => {
+    messages.push(data);
+
+    socket.broadcast.emit("receivedMessage", data);
+  });
 });
 
 server.listen(3000, () => console.log("Servidor Ligado."));
